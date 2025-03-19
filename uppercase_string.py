28761b3294d8a96aa4ed48f51b8d18e4524f4491
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-19 13:13:27 +0800

    Add the code that changes the case of user input to uppercase

--- uppercase_string.py
+++ uppercase_string.py
@@ -8,3 +8,11 @@
 # - input full name
 # - convert to uppercase using upper()
 # - print converted string
+
+# Main Program
+
+full_name = input("Enter your full name: ")
+
+upper_name = full_name.upper()
+
+print(f"Your name in upppercase is {upper_name}")
