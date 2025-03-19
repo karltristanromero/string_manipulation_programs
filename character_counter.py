ebafa47a5b3070492e3329f3e04e6813c6137d69
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-19 23:37:47 +0800

    docs: Add pseudocode to character_counter

--- character_counter.py
+++ character_counter.py
@@ -3,3 +3,10 @@
 # Example:
 # Input: Juan Dela Cruz
 # Output: 14
+
+# Pseudocode
+# - input fullname
+# - replace every spaces with empty string using replace()
+# - set char_counter
+# - count number of characters using for loop
+# - print the number of characters
