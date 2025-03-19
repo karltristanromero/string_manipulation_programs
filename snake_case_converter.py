51d53dcb88aaab1450560fec75be20593e481a51
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-19 23:54:19 +0800

    Add code that converts a messy string to snake casing

--- snake_case_converter.py
+++ snake_case_converter.py
@@ -9,3 +9,12 @@
 # - convert full name to lower case using lower()
 # - replace spaces with _ using replace()
 # - print converted string
+
+# Main Program
+
+messy_full_name = input("Enter your full name in incorrect casing: ")
+
+lower_full_name = messy_full_name.lower()
+snake_case_name = lower_full_name.replace(" ", "_")
+
+print(f"Output: {snake_case_name}")
