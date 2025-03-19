6d4ddc1d31e0ada011eb823da9eafb02e960a3e9
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-19 23:17:58 +0800

    Add code that reverses the case of each letter in the string of the user input

--- reverse_case_string_converter.py
+++ reverse_case_string_converter.py
@@ -8,3 +8,11 @@
 # - input fullname in incorrect casing
 # - convert the casing using swapcase()
 # - print the converted string
+
+# Main Program
+
+messy_full_name = input("Enter your full name in incorrect casing: ")
+
+reverse_case_name = messy_full_name.swapcase()
+
+print(f"I swapped the cases for you: {reverse_case_name}")
