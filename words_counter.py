ac033a8118664f20893c57670b902588302bcf4c
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-19 23:26:09 +0800

    error: Using split to fix the error where the program counts the characters instead of the words

--- words_counter.py
+++ words_counter.py
@@ -6,7 +6,20 @@
 
 # Pseudocode
 # - input string statement
+# - convert string statement into lists using 
 # - set word_total
 # - use for loop to iterate over the statement
 # - append every index
 # - print the word total
+
+# Main Program
+
+complete_statement = input("Enter your thoughts right now: ")
+
+
+word_total = 0
+
+for word in complete_statement:
+    word_total += 1
+
+print(f"Output: {word_total}")
