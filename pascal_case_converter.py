5734e1a191062854059b62713795fbbdb6644ed5
Karl Romero <karltristanfromero@iskolarngbayan.pup.edu.ph>
2025-03-19 23:48:41 +0800

    fix: Change the pseudocode to use replace instead of split in connecting the words into one string

--- pascal_case_converter.py
+++ pascal_case_converter.py
@@ -6,5 +6,12 @@
 
 # Pseudocode
 # - input full name in incorrect casing
-# - convert string into pascal casing using title() and split()
+# - convert string into pascal casing using title() and replace()
 # - print converted string
+
+# Main Program
+
+messy_full_name = input("Enter your full name in incorrect casing: ")
+
+title_case_name = messy_full_name.title()
+pascal_case_name = title_case_name.replace()
